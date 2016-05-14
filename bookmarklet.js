4474c796a99dcc55b38399de64d2eea6e3fe3b3e
Leo <leooscar.simmons@gmail.com>
2016-05-14 00:19:00 -0400

    edits

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,8 +3,8 @@ var refContent = [];
 var refListLength;
 var refListNodes;
 
-refList = document.getElementsByClassName('references').childNodes;
-console.log(refList);
+refListLength = document.getElementsByClassName('references').childNodes.length;
+console.log(refListLength);
 /*refListLength = document.getElementsByClassName('references').childNodes.length;
 for (var i = 0; i < refListLength; i++) {
 	refContent[i] = (refList[i].innerHTML);
