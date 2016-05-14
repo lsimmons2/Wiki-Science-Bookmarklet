b16067eb9a8053ed1416d131194a8d30ed1c352f
Leo <leooscar.simmons@gmail.com>
2016-05-14 11:48:04 -0400

    edits

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,14 @@
-var reflist = [];
+/*var reflist = [];
 var refContent = [];
 var refListLength;
 var refListNodes;
 
-console.log(document.getElementsByClassName('references').childNodes.length;)
+console.log(document.getElementsByClassName('references').childNodes.length;)*/
 /*refListLength = document.getElementsByClassName('references').childNodes.length;
 for (var i = 0; i < refListLength; i++) {
 	refContent[i] = (refList[i].innerHTML);
 }
 
 alert(refContent);*/
+
+alert('yo dog');
