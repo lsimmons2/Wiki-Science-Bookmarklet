e3f4c100df465a60ae01f2ccd40128a00b12f282
Leo <leooscar.simmons@gmail.com>
2016-05-14 13:28:11 -0400

    edits

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,12 @@
-/*var reflist = [];
+var reflist = [];
 var refContent = [];
 var refListLength;
 var refListNodes;
 
-console.log(document.getElementsByClassName('references').childNodes.length;)*/
+console.log(document.getElementsByClassName('references').innerHTML);
 /*refListLength = document.getElementsByClassName('references').childNodes.length;
 for (var i = 0; i < refListLength; i++) {
 	refContent[i] = (refList[i].innerHTML);
 }
 
 alert(refContent);*/
-
-alert(Math.random()*1000);
