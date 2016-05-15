18e7bd59bec58265d8998193e6422ad726495311
Leo <leooscar.simmons@gmail.com>
2016-05-14 23:57:43 -0400

    currently prints out last url of every reference - either pubmed page, or journal's page

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,15 @@
-var reflist = [];
-var refContent = [];
-var refListLength;
-var refListNodes;
+var refElement;
+var refList;
+var pubMed;
+var tags;
 
-console.log(document.getElementsByClassName('references').innerHTML);
-/*refListLength = document.getElementsByClassName('references').childNodes.length;
-for (var i = 0; i < refListLength; i++) {
-	refContent[i] = (refList[i].innerHTML);
+/*refElement is an HTMLCollection of one node- the <ol>*/
+refElement = document.getElementsByClassName('references');
+/*refList if an HMTLCollection of nine nodes - the <li>'s*/
+refList = refElement[0].children;
+/*every refList child - every <li> - is an object */
+/*every <span class="reference-text"> is an HTMLCollection*/
+for (var i = 0; i < refList.length; i++) {
+	tags = refList[i].getElementsByClassName('reference-text')[0].children[0].getElementsByTagName('a');
+	console.log(tags[tags.length-1]);
 }
-
-alert(refContent);*/
