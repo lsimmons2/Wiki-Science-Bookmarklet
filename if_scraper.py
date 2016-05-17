1b2319cfb01560672b6c4fefb122f145b5a87f31
Leo <leooscar.simmons@gmail.com>
2016-05-17 19:37:43 -0400

    Now goes across pages that have the data, have heard nested for loops are bad, but it seems to be fine here? - will look into it

--- if_scraper.py
+++ if_scraper.py
@@ -2,9 +2,6 @@ import lxml
 from lxml import html
 import requests
 
-page1 = requests.get('URL')
-#html.fromstring() takes content as bytes - need to use 'page1.content' and not 'page1.text'
-tree = html.fromstring(page1.content)
 
 class jObj(object):
 	jName = 'No name'
@@ -16,17 +13,26 @@ class jObj(object):
 		self.jName = jName
 		self.if1415 = if1415
 		self.if13 = if13
-		
-jNames = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[2]/text()')
-if1415s = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[4]/text()')
-if13s = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[5]/text()')
-
-jObjs = list()
-for j in range(len(tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]'))):
-	jObjs.append(jObj(jNames[j], if1415s[j], if13s[j]))
-	
-for k in range(len(jObjs)):
-	print jObjs[k].jName, jObjs[k].if1415, jObjs[k].if13, '\n'
 
 
-#print jName[0], if1415[0], if13[0]
+for i in range(1,19):
+	if i == 1:
+		page = requests.get('url.extension')
+	else:
+		page = requests.get('url'+str(i)+'.extension')
+
+	#html.fromstring() takes content as bytes - need to use 'page.content' and not 'page.text'
+	tree = html.fromstring(page.content)
+
+	jNames = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[2]/text()')
+	if1415s = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[4]/text()')
+	if13s = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[5]/text()')
+
+	jObjs = list()
+	for j in range(len(tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]'))):
+		jObjs.append(jObj(jNames[j], if1415s[j], if13s[j]))
+
+	'''
+	for k in range(len(jObjs)):
+		print jObjs[k].jName, jObjs[k].if1415, jObjs[k].if13, '\n'
+	'''
