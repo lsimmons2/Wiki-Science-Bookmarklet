e0d72a44dde601ea3238bc575a56fb21267dc490
Leo <leooscar.simmons@gmail.com>
2016-05-17 18:43:03 -0400

    Web scraper that will take in impact factor data

--- if_scraper.py
+++ if_scraper.py
@@ -3,7 +3,7 @@ from lxml import html
 import requests
 
 page1 = requests.get('URL')
-#html.fromstring() takes content as bytes - need to use 'page1.content' and not page1.text
+#html.fromstring() takes content as bytes - need to use 'page1.content' and not 'page1.text'
 tree = html.fromstring(page1.content)
 
 class jObj(object):
@@ -28,10 +28,5 @@ for j in range(len(tree.xpath('//table[@class="tableizer-table"]/tr[position()>1
 for k in range(len(jObjs)):
 	print jObjs[k].jName, jObjs[k].if1415, jObjs[k].if13, '\n'
 
-'''
-jNameVal = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[2]/text()')
-if1415Val = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[4]/text()')
-if13Val = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[5]/text()')
-'''
 
 #print jName[0], if1415[0], if13[0]
