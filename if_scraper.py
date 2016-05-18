eec071a50875d9e1370406add0b91104d1476edb
Leo <leooscar.simmons@gmail.com>
2016-05-18 04:52:31 -0400

    scrapes impact factors and outputs in a JSON file

--- if_scraper.py
+++ if_scraper.py
@@ -1,19 +1,11 @@
 import lxml
 from lxml import html
 import requests
+import random
+import json
 
 
-class jObj(object):
-	jName = 'No name'
-	if1415 = 0
-	if13 = 0
-
-
-	def __init__(self, jName, if1415, if13):
-		self.jName = jName
-		self.if1415 = if1415
-		self.if13 = if13
-
+dictList = []
 
 for i in range(1,19):
 	if i == 1:
@@ -28,11 +20,13 @@ for i in range(1,19):
 	if1415s = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[4]/text()')
 	if13s = tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]/td[5]/text()')
 
-	jObjs = list()
-	for j in range(len(tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]'))):
-		jObjs.append(jObj(jNames[j], if1415s[j], if13s[j]))
+	for j in range(0,len(tree.xpath('//table[@class="tableizer-table"]/tr[position()>1]'))):
+		dictList.append({'jName':jNames[j], 'if1415':if1415s[j], 'if13':if13s[j]})
 
-	'''
-	for k in range(len(jObjs)):
-		print jObjs[k].jName, jObjs[k].if1415, jObjs[k].if13, '\n'
-	'''
+
+with open('/path/to/file.json', 'a') as outfile:
+	json.dump(dictList, outfile, sort_keys = True, indent=2)
+
+
+
+
